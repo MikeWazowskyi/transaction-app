41bebe29afdaa5c878b9aee348e4f72cb4204358
Михаил Касаткин <m.m.kasatkin@gmail.com>
2023-08-02 13:38:24 +0300

    feature: transaction logic call perform in perform_update method, decorated with transaction.atomic.

--- api/views.py
+++ api/views.py
@@ -21,3 +21,12 @@ class TransactionViewSet(ListUpdateViewSet):
             return UserSerializer(*args, **kwargs)
         if self.request.method == 'PATCH':
             return TransactionSerializers(*args, **kwargs)
+
+    @transaction.atomic
+    def perform_update(self, serializer):
+        sender = self.get_object()
+        validated_data = serializer.validated_data
+        receivers = User.objects.filter(
+            tax_id_number__in=validated_data['receivers'])
+        perform_transactions(sender, receivers, validated_data)
+        super().perform_update(serializer)
